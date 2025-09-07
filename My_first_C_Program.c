c5de7fc7560827a7fbd34a5d15e0c675f8f15a0f
iTheEpics <ihsanegebolgesi@gmail.com>
2025-09-07 21:13:40 +0200

    My_first_C_Program

---
+++ My_first_C_Program.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main(){
+   
+    int num1;
+    int num2;
+    char op;
+
+
+        printf("Enter a number: ");
+    if (scanf("%d", &num1) != 1) {
+        printf("Please enter a valid number.\n");
+    }
+    
+    printf("Enter an operator: ");
+    scanf(" %c", &op);
+    if (op !='+' && op !='-' && op != '/' && op != '*')
+    {
+        printf("Please enter a valid operator.\n");
+        return 1;
+    }
+    
+    printf("Enter a number: ");
+    if(scanf("%d", &num2) != 1){
+        printf("Please enter a valid number.\n");
+    }
+    
+    if (op == '+')
+    {
+        printf("%d\n", num1 + num2);
+    }
+    else if (op == '-')
+    {
+        printf("%d\n", num1 - num2);
+    }
+    else if (op == '/')
+    {
+        printf("%d\n", num1 / num2);
+    }
+    else if (op == '*')
+    {
+        printf("%d\n", num1 * num2);
+    }
+
+}
+
+
